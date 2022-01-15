cc38ccf6507f8db5bd6efc43decdc768bc479244
Pawan Kumar <singhpawank12@gmail.com>
2022-01-15 16:23:49 +0530

    Update assignment 1

--- Assignment-1/assignment-1.py
+++ Assignment-1/assignment-1.py
@@ -1,9 +1,16 @@
 import sys
-import random
+import uuid
+import time
 import datetime
 
-def uniqueId(username, dt):
-    print("Unique Id:", random.randint(100000, 999999))
+def generate_uniqueId():
+
+    x = uuid.uuid4()
+    ms = time.time_ns()
+    return uuid.uuid5(x, str(ms))
+
+def assignment1(username, dt):
+    print("Unique Id:", generate_uniqueId())
     print("User name:", username)
     print("Date & Time: {}-{}-{} {}:{}:{} {}".format(dt.day, dt.strftime("%b"), dt.year, dt.strftime("%I"), dt.strftime("%M"), dt.strftime("%S"), dt.strftime("%p")))
 
@@ -12,7 +19,7 @@ def main():
         print("---Please enter user name---")
 
     else:
-     uniqueId(sys.argv[1], datetime.datetime.now())
+     assignment1(sys.argv[1], datetime.datetime.now())
 
 
 if __name__ == "__main__":
